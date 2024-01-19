2f04b331e80eca852b88bc156d426bd8681e75cf
foruzinsky <filip.oruzinsky@t-systems.com>
2024-01-19 12:43:30 +0100

    First commit

---
+++ src/main/java/com/k3project/demo/BarkingAppApplication.java
@@ -0,0 +1,13 @@
+package com.k3project.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BarkingAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BarkingAppApplication.class, args);
+	}
+
+}
