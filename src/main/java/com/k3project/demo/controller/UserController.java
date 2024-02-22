123e1910835d9a7996d835f040242317103b0673
foruzinsky <filip.oruzinsky@t-systems.com>
2024-02-22 10:00:49 +0100

    KAN-13 --> endpoint fixed

--- src/main/java/com/k3project/demo/controller/UserController.java
+++ src/main/java/com/k3project/demo/controller/UserController.java
@@ -11,8 +11,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
-
-
 import java.util.*;
 
 @Tag(name = "User Management", description = "APIs for managing users")
